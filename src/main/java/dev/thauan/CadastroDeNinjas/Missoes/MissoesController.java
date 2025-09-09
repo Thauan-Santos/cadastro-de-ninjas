02887b27e5a9685ae2707e72b6d32d99658b02fa
Thauan Santos <Thauan.Santos01@outlook.com>
2025-09-09 16:50:25 -0300

    fix: altera o nome das rotas do controller dos ninjas

--- src/main/java/dev/thauan/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/thauan/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,4 +1,31 @@
 package dev.thauan.CadastroDeNinjas.Missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
 public class MissoesController {
+
+    @GetMapping("/listar")
+    public String listarMissao(){
+        return "Missoes listadas";
+    }
+
+
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "Missao criada";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao(){
+        return "Missao alterada";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarMissao(){
+        return "Missao deletada";
+    }
+
+
 }
