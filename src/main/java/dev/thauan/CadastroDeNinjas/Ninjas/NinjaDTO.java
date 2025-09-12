40850b424b71b505c62610c0332b8d8748ca7aa4
Thauan Santos <Thauan.Santos01@outlook.com>
2025-09-12 17:19:34 -0300

    chore: inclui a versão do dto pela entidade e mapeia manualmente no NinjaMapper os caminhos x dto

--- src/main/java/dev/thauan/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/dev/thauan/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,20 @@
 package dev.thauan.CadastroDeNinjas.Ninjas;
+import dev.thauan.CadastroDeNinjas.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private String rank;
+    private MissoesModel missoes;
+
+
 }
